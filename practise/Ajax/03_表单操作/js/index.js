840cde5568438a04486c01e2d688ff32eb91ddef
xrswyea <cjy1943222010@qq.com>
2022-08-14 22:57:13 +0800

    模板引擎

--- practise/Ajax/03_表单操作/js/index.js
+++ practise/Ajax/03_表单操作/js/index.js
@@ -1,73 +1,47 @@
-let tbody = document.querySelector("tbody");
-let btn_addBook = document.querySelector(".btn_addBook");
-let iptBookname = document.querySelector("#iptBookname");
-let iptAuthor = document.querySelector("#iptAuthor");
-let iptPublisher = document.querySelector("#iptPublisher");
-function getBooks() {
-    //发起Ajax请求
+let cmtList = document.querySelector(".list-group");
+/**
+ * @type{HTMLFormElement} form
+ */
+let form = document.querySelector(".sendCmt");
+function getCmt() {
+    cmtList.innerHTML = "";
     $.ajax({
         type: "get",
-        url: "http://www.liulongbin.top:3006/api/getbooks",
+        url: "http://www.liulongbin.top:3006/api/cmtlist",
         success: function (response) {
-            tbody.innerHTML = "";
-            if (response.status != 200) {
-                alert("读取出错");
-            } else {
-                //遍历添加数据, 添加删除按钮
+            console.log(response);
+            if (response.status == 200) {
                 response.data.forEach((element) => {
-                    let { id, bookname, author, publisher } = element;
-                    let html = `
-                                <tr>
-                                    <td class="id">${id}</td>
-                                    <td>${bookname}</td>
-                                    <td>${author}</td>
-                                    <td>${publisher}</td>
-                                    <td><button type="button" class="btn btn-danger btn-del">删除图书</button></td>
-
-                                </tr>
-                                `;
-                    tbody.insertAdjacentHTML("beforeend", html);
+                    console.log(element);
+                    // element: "id": 34,
+                    //          "username": "丫丫",
+                    //          "content": "123456",
+                    //          "time": "2022-08-14 12:44:32"
+                    let { id, username, content, time } = element;
+                    let html = `<li class="list-group-item">
+                                    <span class="badge" style="background-color: #eeac4e">评论时间: ${time}</span>
+                                    <span class="badge" style="background-color: #5abfdb">评论人: ${username}</span>
+                                    ${content}
+                                </li>`;
+                    cmtList.insertAdjacentHTML("beforeend", html);
                 });
             }
-            // 给删除按钮绑定事件
-            $(".btn-del").on("click", function (param) {
-                console.log($(this).parent().siblings(".id").text());
-                delBook($(this).parent().siblings(".id").text());
-                getBooks();
-            });
         },
     });
 }
-function delBook(del_id) {
-    $.ajax({
-        type: "get",
-        url: "http://www.liulongbin.top:3006/api/delbook",
-        data: {
-            id: del_id,
-        },
-        success: function (response) {
-            console.log(response);
-        },
-    });
-}
+getCmt();
 
-function addBook(bookname, author, publisher) {
+$(".sendCmt").on("submit", function (e) {
+    e.preventDefault();
+    console.log($(".sendCmt").serialize());
     $.ajax({
         type: "post",
-        url: "http://www.liulongbin.top:3006/api/addbook",
-        data: {
-            bookname: bookname,
-            author: author,
-            publisher: publisher,
-        },
+        url: "http://www.liulongbin.top:3006/api/addcmt",
+        data: $(".sendCmt").serialize(),
         success: function (response) {
             console.log(response);
-            getBooks();
+            getCmt();
+            form.reset();
         },
     });
-}
-getBooks();
-$(btn_addBook).on("click", function () {
-    console.log("开始添加图书");
-    addBook(iptBookname.value, iptAuthor.value, iptPublisher.value);
 });
