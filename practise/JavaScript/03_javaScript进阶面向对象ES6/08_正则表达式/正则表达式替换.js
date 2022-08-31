8586a87aef390789a606ec426f4d4a2e81f22cdf
xrswyea <cjy1943222010@qq.com>
2022-08-31 22:32:10 +0800

    开发自己的包

--- practise/JavaScript/03_javaScript进阶面向对象ES6/08_正则表达式/正则表达式替换.js
+++ practise/JavaScript/03_javaScript进阶面向对象ES6/08_正则表达式/正则表达式替换.js
@@ -1,5 +1,7 @@
 let str =
     "大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛大牛子小牛子牛子牛子牛牛牛牛";
 console.log(str.match(/牛子/g));
-str = str.replace(/牛子/g, "**");
+str = str.replace(/牛子/g, function (match) {
+    return "**";
+});
 console.log(str);
