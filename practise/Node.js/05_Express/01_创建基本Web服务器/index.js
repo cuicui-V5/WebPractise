96c011d8714d7a75f79151ed2dbc77db2e962a53
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-09-01 16:02:17 +0800

    express邝小姐

--- practise/Node.js/05_Express/01_创建基本Web服务器/index.js
+++ practise/Node.js/05_Express/01_创建基本Web服务器/index.js
@@ -2,6 +2,27 @@
 const express = require("express");
 // 创建服务器
 const server = express();
+//监听get请求
+server.get("/", function (req, res) {
+    // 处理函数
+    console.log("收到了一个get请求");
+    // 获取查询字符串
+    console.log(req.query);
+
+    res.send("get 请求成功");
+});
+// 监听post请求
+server.post("/", function (req, res) {
+    // 处理函数
+    console.log("收到了一个post请求");
+    res.send("post请求成功");
+});
+
+// 获取动态参数
+server.get("/user/:id/:name", function (req, res) {
+    res.send(req.params);
+});
+
 // 开启服务器
 server.listen(80, () => {
     console.log("server started at http://127.0.0.1");
