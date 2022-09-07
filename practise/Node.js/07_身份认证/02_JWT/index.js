d2ecb8e930bea890333a357ae1b62d0df0937dfd
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-09-07 16:33:41 +0800

    1

--- practise/Node.js/07_身份认证/02_JWT/index.js
+++ practise/Node.js/07_身份认证/02_JWT/index.js
@@ -8,9 +8,12 @@ const express = require("express");
 const app = express();
 
 const secretKey = ",/.,/.,.,3123";
+// jwt中间件 解析jwt为json
 app.use(expressjwt({ secret: secretKey, algorithms: ["HS256"] }).unless({ path: [/^\/login/] }));
+
 app.use(express.json());
 app.post("/login", (req, res) => {
+    // 生成jwt
     const token = jwt.sign(req.body, secretKey, {
         expiresIn: "1000s",
     });
@@ -20,6 +23,16 @@ app.post("/login", (req, res) => {
 app.get("/user", (req, res) => {
     res.send(req.auth.name);
     console.log(req.auth.name);
+});
+
+// 错误中间件
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.send({
+        status: 1,
+        msg: err.message,
+    });
+    next();
 });
 
 app.listen(80, () => {
