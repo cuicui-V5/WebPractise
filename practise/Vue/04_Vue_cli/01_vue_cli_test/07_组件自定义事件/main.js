fe39245e20f35304095da0a579899e88c4a1467a
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-10-17 11:21:36 +0800

    组件自定义事件

---
+++ practise/Vue/04_Vue_cli/01_vue_cli_test/07_组件自定义事件/main.js
@@ -0,0 +1,7 @@
+import App from "./App.vue";
+import Vue from "vue";
+
+new Vue({
+    el: ".root",
+    render: h => h(App),
+});
---
+++ practise/Vue/04_Vue_cli/01_vue_cli_test/src/components/MySchool.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1 id="school">学校名称是 {{ name }}</h1>
+        <button @click="emit">btn</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MySchool",
+    data() {
+        return {
+            name: "xx小学",
+        };
+    },
+    methods: {
+        emit() {
+            this.$emit("myEvent", "hello");
+        },
+    },
+};
+</script>
+
+<style>
+#school {
+    color: violet;
+
+    font-size: 20px;
+}
+</style>
