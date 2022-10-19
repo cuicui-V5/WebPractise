1d37cb65081ef1415b3e961fb894510d075f0384
xrswyea <cjy1943222010@qq.com>
2022-10-19 21:04:03 +0800

    消息订阅与发布, nextTick 过渡与动画

---
+++ practise/Vue/04_Vue_cli/01_vue_cli_test/13_动画和过渡/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="root">
+        动画方式实现
+        <test></test>
+        过度方式实现
+        <test2></test2>
+    </div>
+</template>
+
+<script>
+import test from "./components/MyTest.vue";
+import test2 from "./components/MyTest2.vue";
+export default {
+    name: "App",
+    components: {
+        test,
+        test2,
+    },
+};
+</script>
---
+++ practise/Vue/04_Vue_cli/01_vue_cli_test/13_动画和过渡/components/MyTest2.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="show = !show">点我切换</button>
+        <transition-group appear name="my">
+            <h1 v-show="show" key="1">hello vue</h1>
+            <h1 v-show="!show" key="2">hello vue</h1>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            show: true,
+        };
+    },
+};
+</script>
+
+<style lang="less" scoped>
+h1 {
+    background-color: rgb(219, 219, 180);
+}
+.my-enter,
+.my-leave-to {
+    transform: translateX(-100%);
+}
+.my-leave,
+.my-enter-to {
+    transform: translateX(0);
+}
+.my-enter-active,
+.my-leave-active {
+    transition: 1s;
+}
+</style>
