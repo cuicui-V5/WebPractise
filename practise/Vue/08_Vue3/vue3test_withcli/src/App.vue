66ac7e954fffc63f6d2bf21657dd54c7d686a814
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-11-01 18:00:30 +0800

    vue3

---
+++ practise/Vue/08_Vue3/vue3test_withcli/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>welcome to vue3</h1>
+    <h2>name:{{ name }}</h2>
+    <h2>name:{{ age }}</h2>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {},
+    setup() {
+        let name = "tim";
+        let age = 18;
+        return {
+            name,
+            age,
+        };
+    },
+};
+</script>
+
+<style></style>
