7ce8b046b6ce80e47078a881b2d309240a5d59e2
xrswyea <cjy1943222010@qq.com>
2022-11-01 20:49:43 +0800

    Vue3环境搭建, setup函数, ref函数实现响应式

--- practise/Vue/08_Vue3/vue3test_withcli/src/App.vue
+++ practise/Vue/08_Vue3/vue3test_withcli/src/App.vue
@@ -1,19 +1,26 @@
 <template>
     <h1>welcome to vue3</h1>
     <h2>name:{{ name }}</h2>
-    <h2>name:{{ age }}</h2>
+    <h2>age:{{ age }}</h2>
+    <button @click="clickAdd">点我+1 age={{ age }}</button>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
     name: "App",
     components: {},
     setup() {
-        let name = "tim";
-        let age = 18;
+        let name = ref("tim");
+        let age = ref(18);
+        function clickAdd() {
+            age.value++;
+            console.log(age);
+        }
         return {
             name,
             age,
+            clickAdd,
         };
     },
 };
