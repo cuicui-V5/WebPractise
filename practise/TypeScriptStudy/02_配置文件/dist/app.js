6f3556060d577f2439a33e2f48ec5f58b13e96c5
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-11-08 16:25:17 +0800

    ts配置文件

---
+++ practise/TypeScriptStudy/02_配置文件/dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+let a;
+a = 1;
+let b;
+b = 100;
+b = "123";
+let fun = function () {
+    console.log(1);
+    return 1;
+};
+let box = document.querySelector("selectors");
+box === null || box === void 0 ? void 0 : box.addEventListener("click", fun);
---
+++ practise/TypeScriptStudy/02_配置文件/src/app.ts
@@ -0,0 +1,16 @@
+let a: number;
+a = 1;
+
+let b;
+
+b = 100;
+b = "123";
+
+let fun = function () {
+    console.log(1);
+    return 1;
+};
+
+let box = document.querySelector("selectors");
+
+box?.addEventListener("click", fun);
