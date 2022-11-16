479d0157b285b7d514d1475068b1b6135c2f3e55
xrswyea <cjy1943222010@qq.com>
2022-11-16 19:13:19 +0800

    ws

--- practise/JavaScript/04_WebSocket/WsServer/index.js
+++ practise/JavaScript/04_WebSocket/WsServer/index.js
@@ -1,23 +1,26 @@
 import { WebSocketServer } from "ws";
 const server = new WebSocketServer({ port: 1234 });
 
+const obj = {
+    msg: "hello from server",
+    code: "200",
+};
+
 server.on("connection", (socket) => {
     // 向客户端发送消息
-    socket.send(
-        JSON.stringify({
-            msg: "hello from server",
-            code: "200",
-        }),
-    );
+    socket.send(JSON.stringify(obj));
 
     // 从客户端接收消息
     socket.on("message", (data) => {
-        console.log(data);
         const packet = JSON.parse(data);
+        obj.msg = packet.msg;
+        boardCast();
         console.log(packet);
     });
-
-    socket.addEventListener("message", ({ data }) => {
-        console.log(data);
-    });
 });
+
+function boardCast() {
+    server.clients.forEach((client) => {
+        client.send(JSON.stringify(obj));
+    });
+}
