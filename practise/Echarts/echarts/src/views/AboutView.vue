d1cf9b0a4f1f61cbf32131e4a9fe9268e183b92d
xrswyea <cjy1943222010@qq.com>
2022-11-29 22:21:55 +0800

    开始学习echarts

---
+++ practise/Echarts/echarts/src/views/AboutView.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="about">
+        <!-- echarts的容器, 必须设置宽高, 否则不显示 -->
+        <div
+            class="echarts"
+            ref="ec"
+        ></div>
+    </div>
+</template>
+<script lang="ts" setup>
+    // 引入echarts
+    import * as echarts from "echarts";
+    import { ref, onMounted } from "vue";
+    const ec = ref<HTMLElement | null>(null);
+    onMounted(() => {
+        console.log("111");
+        console.log(ec.value);
+
+        if (ec.value != null) {
+            const myEcharts = echarts.init(ec.value);
+            myEcharts.setOption({
+                // 设置参数
+                title: {
+                    text: "ECharts 入门示例",
+                },
+                tooltip: {},
+                legend: {
+                    data: ["销量"],
+                },
+                xAxis: {
+                    data: [
+                        "衬衫",
+                        "羊毛衫",
+                        "雪纺衫",
+                        "裤子",
+                        "高跟鞋",
+                        "袜子",
+                    ],
+                },
+                yAxis: {},
+                series: [
+                    {
+                        name: "销量",
+                        type: "bar",
+                        data: [5, 20, 36, 10, 10, 20],
+                    },
+                ],
+            });
+        }
+    });
+</script>
+
+<style lang="scss">
+    @media (min-width: 1024px) {
+        .about {
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+        }
+    }
+    .echarts {
+        width: 400px;
+        height: 600px;
+    }
+</style>
