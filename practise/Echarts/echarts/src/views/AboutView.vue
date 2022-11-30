0b797be1122218703b92a8e820e6b940207e895a
xrswyea <cjy1943222010@qq.com>
2022-11-30 22:36:39 +0800

    1

--- practise/Echarts/echarts/src/views/AboutView.vue
+++ practise/Echarts/echarts/src/views/AboutView.vue
@@ -8,44 +8,99 @@
     </div>
 </template>
 <script lang="ts" setup>
-    // 引入echarts
-    import * as echarts from "echarts";
+    import * as echarts from "echarts/core";
+    import {
+        BarChart,
+        // 系列类型的定义后缀都为 SeriesOption
+        type BarSeriesOption,
+        LineChart,
+        type LineSeriesOption,
+    } from "echarts/charts";
+    import {
+        TitleComponent,
+        // 组件类型的定义后缀都为 ComponentOption
+        type TitleComponentOption,
+        TooltipComponent,
+        type TooltipComponentOption,
+        GridComponent,
+        type GridComponentOption,
+        // 数据集组件
+        DatasetComponent,
+        type DatasetComponentOption,
+        // 内置数据转换器组件 (filter, sort)
+        TransformComponent,
+    } from "echarts/components";
+    import { LabelLayout, UniversalTransition } from "echarts/features";
+    import { CanvasRenderer } from "echarts/renderers";
     import { ref, onMounted } from "vue";
+
+    // 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
+    type ECOption = echarts.ComposeOption<
+        | BarSeriesOption
+        | LineSeriesOption
+        | TitleComponentOption
+        | TooltipComponentOption
+        | GridComponentOption
+        | DatasetComponentOption
+    >;
+    // 注册必须的组件
+    echarts.use([
+        TitleComponent,
+        TooltipComponent,
+        GridComponent,
+        DatasetComponent,
+        TransformComponent,
+        BarChart,
+        LineChart,
+        LabelLayout,
+        UniversalTransition,
+        CanvasRenderer,
+    ]);
     const ec = ref<HTMLElement | null>(null);
     onMounted(() => {
-        console.log("111");
-        console.log(ec.value);
-
-        if (ec.value != null) {
-            const myEcharts = echarts.init(ec.value);
-            myEcharts.setOption({
-                // 设置参数
-                title: {
-                    text: "ECharts 入门示例",
+        const option: ECOption = {
+            title: {
+                text: "标题",
+                subtext: "副标题",
+                left: "center",
+            },
+            tooltip: {
+                // 提示框触发方式: item 只有在图形上才会触发, axis在范围内都会触发
+                trigger: "axis",
+                // 坐标轴指示器 line 线, shadow 阴影, cross , 十字线准星效果
+                axisPointer: {
+                    type: "shadow",
                 },
-                tooltip: {},
-                legend: {
-                    data: ["销量"],
+                // 是否显示提示框
+                showContent: true,
+                // 样式
+                backgroundColor: "pink",
+                textStyle: {
+                    fontSize: "20",
                 },
-                xAxis: {
-                    data: [
-                        "衬衫",
-                        "羊毛衫",
-                        "雪纺衫",
-                        "裤子",
-                        "高跟鞋",
-                        "袜子",
-                    ],
-                },
-                yAxis: {},
-                series: [
-                    {
-                        name: "销量",
-                        type: "bar",
-                        data: [5, 20, 36, 10, 10, 20],
-                    },
+                // 内容格式化
+                // formatter: (p) => {
+                //     return "value=" + p;
+                // },
+            },
+            xAxis: {
+                data: ["1", "2", "3"],
+            },
+            yAxis: {},
+            series: {
+                name: "test",
+                type: "bar",
+                data: [
+                    { value: 100, name: "123" },
+                    { value: 300, name: "2929-2-1" },
+                    { value: 300, name: "2929-2-1" },
                 ],
-            });
+            },
+        };
+        // 接下来的使用就跟之前一样，初始化图表，设置配置项
+        if (ec.value !== null) {
+            var myChart = echarts.init(ec.value);
+            myChart.setOption(option);
         }
     });
 </script>
@@ -59,7 +114,7 @@
         }
     }
     .echarts {
-        width: 400px;
+        width: 100%;
         height: 600px;
     }
 </style>
