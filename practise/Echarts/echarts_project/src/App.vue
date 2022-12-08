516dc37c863213f29f9cf5caee68f38ab6c3baf7
cuicuiV5_sp7 <cjy1943222010@qq.com>
2022-12-08 11:36:04 +0800

    init echarts project

---
+++ practise/Echarts/echarts_project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <RouterView />
+</template>
+<style lang="less">
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        background-image: url(./assets/bg.jpeg);
+        background-size: cover;
+    }
+</style>
---
+++ practise/Echarts/echarts_project/src/views/home.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>echarts数据可视化</h1>
+    </header>
+    <section class="container">
+        <div class="left"></div>
+        <div class="center"></div>
+        <div class="right"></div>
+    </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+    header {
+        height: 5vw;
+        background-color: #ced0dc25;
+        backdrop-filter: blur(50%);
+        h1 {
+            line-height: 5vw;
+            font-size: 2vw;
+            text-align: center;
+        }
+    }
+    .container {
+        border: 2px solid black;
+        min-width: 1200px;
+        max-width: 2560px;
+        padding: 1vw 0 0 0;
+        background-color: skyblue;
+        display: flex;
+        .left,
+        .right {
+            flex: 3;
+        }
+        .center {
+            flex: 5;
+        }
+    }
+</style>
