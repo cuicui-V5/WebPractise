e570a3185a35d8e1b2b776e759655faf466834cf
xrswyea <cjy1943222010@qq.com>
2022-12-28 23:22:34 +0800

    学习压缩图片

---
+++ practise/JavaScript/03_javaScript进阶面向对象ES6/上传图片压缩/backend/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import multer from "multer";
+import cors from "cors";
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname +
+                "-" +
+                Date.now() +
+                "." +
+                file.originalname.split(".")[
+                    file.originalname.split(".").length - 1
+                ],
+        );
+    },
+});
+const upload = multer({ dest: "uploads/", storage });
+
+const app = express();
+app.use(cors());
+
+app.post("/upload", upload.single("avatar"), function (req, res) {
+    // req.file 是 `avatar` 文件的信息
+    console.log(req.file);
+    // req.body 将具有文本域数据，如果存在的话
+    res.send({
+        msg: "ok",
+    });
+});
+
+app.post(
+    "/photos/upload",
+    upload.array("photos", 12),
+    function (req, res, next) {
+        // req.files 是 `photos` 文件数组的信息
+        // req.body 将具有文本域数据，如果存在的话
+    },
+);
+
+app.get("/", function (res, req) {
+    console.log("get");
+    req.send("hello");
+});
+
+app.listen(8888, "127.0.0.1", () => {
+    console.log("server started at http://127.0.0.1:8888");
+});
---
+++ practise/JavaScript/03_javaScript进阶面向对象ES6/上传图片压缩/frontend/src/原始版本 分成两个promise.ts
@@ -0,0 +1,114 @@
+// 上传图片压缩的步骤
+// 1. 获取input数据, 使用fileReader转换成dataUrl
+// 2. 将dataUrl的值赋给image的src
+// 3. 将image绘制进canvas
+// 4. 使用canvas.toBlob获取blob对象, 可以在其中设置压缩比例
+// 5. 上传图片
+import axios from "axios";
+
+const fileToImg = (file: File): Promise<HTMLImageElement> => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        const img = new Image();
+        // 将传进来的file转换成dataURl
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            img.src = reader.result as string;
+        };
+        reader.onerror = (e) => {
+            reject(e);
+        };
+        img.onload = () => {
+            resolve(img);
+        };
+        img.onerror = (e) => {
+            reject(e);
+        };
+    });
+};
+
+const imgToBlob = (
+    img: HTMLImageElement,
+    maxWidth: number,
+    maxHeight: number,
+    quality: number,
+    type = "image/png",
+): Promise<Blob> => {
+    return new Promise((resolve, reject) => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        // 获取图片原始尺寸
+        const { width: originWidth, height: originHeight } = img;
+        // 确定目标尺寸
+        let targetWidth = originWidth,
+            targetHeight = originHeight;
+        // 如果图片尺寸大于目标尺寸, 那么就进行等比缩放
+        if (targetWidth > maxWidth || targetHeight > maxHeight) {
+            // 等比缩小的算法
+            // 如果是横着的图, 图片的宽等于最大宽度, 高等比缩放;
+            // 如果是竖着的图, 图片的高等于最大高度, 宽等比缩放;
+            // 先判断是横着的图还是竖着的图
+            if (targetWidth > targetHeight) {
+                //横着的
+                targetWidth = maxWidth;
+                targetHeight = targetHeight * (maxWidth / originWidth);
+            } else {
+                //竖着的
+                targetHeight = maxHeight;
+                targetWidth = targetWidth * (maxHeight / originHeight);
+            }
+        }
+        try {
+            // 把img绘制进canvas
+            canvas.width = targetWidth;
+            canvas.height = targetHeight;
+            ctx?.clearRect(0, 0, targetWidth, targetHeight);
+            ctx?.drawImage(img, 0, 0, targetWidth, targetHeight);
+            document.body.append(canvas);
+            // 获取blob对象
+            canvas.toBlob(
+                (blob) => {
+                    if (blob) {
+                        resolve(blob);
+                    } else {
+                        reject(new Error("转换失败"));
+                    }
+                },
+                type,
+                quality,
+            );
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+let blobToSend: Blob;
+
+const inp = document.querySelector("#input");
+inp?.addEventListener("change", async (e) => {
+    const inputEle = e.target as HTMLInputElement;
+    if (inputEle.files?.length) {
+        const img = await fileToImg(inputEle.files[0]);
+
+        const blob = await imgToBlob(img, 1000, 1000, 0.2);
+
+        blobToSend = blob;
+        console.log(blobToSend);
+    }
+});
+
+const btn = document.querySelector("#btn");
+btn?.addEventListener("click", () => {
+    const fd = new FormData();
+    fd.append("avatar", blobToSend, "avatar.png");
+    axios
+        .post("http://127.0.0.1:8888/upload", fd)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
